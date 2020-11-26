0aabde98220ac79e80ead6c10aec4172ac1caa76
Andrescortes23 <andresccortes23@icloud.com>
2020-11-26 15:23:12 +0000

    Task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,13 +1,16 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
- *
+ *binary_tree_insert_right - inser new node to right
+ *@parent: parent of ne node
+ *@value: number inside new node
+ *Return: new node
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
 
 	new = binary_tree_node(parent, value);
 	if (new == NULL)
@@ -23,5 +26,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new->right = parent->right;
 		parent->right = new;
 	}
-	return new;
+	return (new);
 }
