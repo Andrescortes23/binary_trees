a16e4a9ec36c5466db4b87761a70fccabb46068b
Andrescortes23 <andresccortes23@icloud.com>
2020-11-26 15:18:34 +0000

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
- *
+ *binary_tree_insert_left - insert node to left
+ *@parent: parent of new node
+ *@value: number in new node
+ *Return: new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -23,5 +23,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new->left = parent->left;
 		parent->left = new;
 	}
-	return new;
+	return (new);
 }
