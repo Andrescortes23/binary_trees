165cf22512d8eac5406e3815551dbe46d971a77d
Andrescortes23 <andresccortes23@icloud.com>
2020-11-27 00:02:05 +0000

    Task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,11 +9,10 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || func == NULL)
 		return;
 
-	func(tree->n);
-
 	if (tree)
 	{
 		binary_tree_inorder(tree->left, func);
+		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
 }
